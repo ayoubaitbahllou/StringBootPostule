2edefc32c9ca447772d149752730ef2a5d62483d
ayoub aitbahllou <ayoub.aitbahllou@hotmail.com>
2022-01-15 13:13:36 +0100

    master : Add uploading file(CV)

--- src/main/java/com/offre/employee/controllers/UserController.java
+++ src/main/java/com/offre/employee/controllers/UserController.java
@@ -3,10 +3,12 @@ package com.offre.employee.controllers;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.offre.employee.models.User;
 import com.offre.employee.repositories.UserRepository;
+import com.offre.employee.services.UploadFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
@@ -23,15 +25,24 @@ public class UserController {
 
     @CrossOrigin(origins = "http://localhost:4200/")
     @PostMapping("/register")
-    public ResponseEntity<HashMap> register(@RequestParam Map<String, String> inputs) {
+    public ResponseEntity<HashMap> register(@RequestParam("cv") MultipartFile file, @RequestParam Map<String, String> inputs) {
         User user = new User();
         user.setEmail(inputs.get("email"));
         user.setName(inputs.get("name"));
         user.setPassword(inputs.get("password"));
+        user.setCv(this.uploadCv(file));
         userRepository.save(user);
         HashMap<String, String> map = new HashMap<>();
         map.put("message","Vous etes bien enregistrer");
         return new ResponseEntity<HashMap>(map, HttpStatus.OK);
+    }
+
+    public String uploadCv(MultipartFile file) {
+        String location = "C:\\Users\\Meriem\\projects\\masterCours\\StringBootPostule\\src\\main\\uploads";
+        UploadFileService uploadFileService = new UploadFileService(location);
+        uploadFileService.saveFile(file);
+        //
+        return uploadFileService.resPath;
     }
 
     @CrossOrigin(origins = "http://localhost:4200/")
